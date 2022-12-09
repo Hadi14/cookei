cec8d56f78def63ba59d7138c5f785508a7118d4
Hadi14 <asusn43@yahoo.com>
2022-12-09 16:41:20 +0330

    ok1

--- js/login.js
+++ js/login.js
@@ -4,30 +4,31 @@ const passwordInput = $.querySelector('#password')
 const rememberMeCheckbox = $.querySelector('.ck')
 const loginBtn = $.querySelector('button')
 
-function setCookie (cookieName, cookieValue, exDay) {
+function setCookie(cookieName, cookieValue, exDay) {
     const now = new Date()
-    
+
     now.setTime(now.getTime() + exDay * 24 * 60 * 60 * 1000)
 
     document.cookie = `${cookieName}=${cookieValue};path=/;expires=${now}`
+    // document.Cookei = `username=${username.value};path=/;expires=${now}`;
 }
 
-function getCookie (cookieName) {
-    let cookiesArray = document.cookie.split(';')
+// function getCookie (cookieName) {
+//     let cookiesArray = document.cookie.split(';')
 
-    let mainCookie = null
+//     let mainCookie = null
 
-    cookiesArray.some(cookie => {
-        if (cookie.includes(cookieName)) {
-            mainCookie = cookie.substring(cookie.indexOf('=') + 1)
-            return true
-        }
-    })
+//     cookiesArray.some(cookie => {
+//         if (cookie.includes(cookieName)) {
+//             mainCookie = cookie.substring(cookie.indexOf('=') + 1)
+//             return true
+//         }
+//     })
 
-    return mainCookie
-}
+//     return mainCookie
+// }
 
-function clearInput () {
+function clearInput() {
     usernameInput.value = ''
     passwordInput.value = ''
 }
